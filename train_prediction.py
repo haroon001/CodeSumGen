751d6aac626dd5a31a4039dac08c44db7a4ce2b3
Matt DiRisio <dirisiom@msu.edu>
2024-11-01 12:33:48 -0400

    loaded py150 dataset

--- train_prediction.py
+++ train_prediction.py
@@ -1,7 +1,7 @@
 from datasets import load_dataset
 from transformers import (
-    T5ForConditionalGeneration,
-    RobertaTokenizer,
+    AutoModelForCausalLM,
+    AutoTokenizer,
     Seq2SeqTrainingArguments,
     Seq2SeqTrainer,
     DataCollatorForSeq2Seq
@@ -13,3 +13,14 @@ from sklearn.model_selection import train_test_split
 from huggingface_hub import login
 
 login('')
+cuda = torch.cuda.is_available()
+mps = hasattr(torch.backends, "mps") and torch.backends.mps.is_available()
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/CodeGPT-small-py")
+model = AutoModelForCausalLM.from_pretrained("microsoft/CodeGPT-small-py")
+
+dataset = load_dataset("eth_py150_open")
+
+train_dataset = dataset["train"]
+test_dataset = dataset["test"]
+validation_dataset = dataset["validation"]
